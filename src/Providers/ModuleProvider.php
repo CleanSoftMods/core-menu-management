ba3ef07be03265bff2e283f85fdd225510a3727e
Tedozi Manson <duyphan.developer@gmail.com>
2016-11-28 09:57:52 +0700

    Refactor

---
+++ src/Providers/ModuleProvider.php
@@ -0,0 +1,50 @@
+<?php namespace WebEd\Base\Menu\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use WebEd\Base\Menu\Facades\DashboardMenuFacade;
+use WebEd\Base\Menu\Facades\MenuManagementFacade;
+
+class ModuleProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /*Load views*/
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'webed-menu');
+        /*Load translations*/
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'webed-menu');
+        /*Load migrations*/
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+        $this->publishes([
+            __DIR__ . '/../../resources/views' => config('view.paths')[0] . '/vendor/webed-menu',
+        ], 'views');
+        $this->publishes([
+            __DIR__ . '/../../resources/lang' => base_path('resources/lang/vendor/webed-menu'),
+        ], 'lang');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //Load helpers
+        load_module_helpers(__DIR__);
+
+        $this->app->register(RouteServiceProvider::class);
+        $this->app->register(RepositoryServiceProvider::class);
+        $this->app->register(BootstrapModuleServiceProvider::class);
+
+        //Register related facades
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('MenuManagement', MenuManagementFacade::class);
+        $loader->alias('DashboardMenu', DashboardMenuFacade::class);
+    }
+}
